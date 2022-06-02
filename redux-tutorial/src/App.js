25f4b9c0925bafe3f48ca9a7190b3fd64833373b
Andries Felix <efix66@gmail.com>
2022-06-02 10:42:22 +0300

    update

--- redux-tutorial/src/App.js
+++ redux-tutorial/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Counter from "./features/counter/Counter";
 
 function App() {
 	return (
 		<div className="App">
 			<h1>React Redux</h1>
+			<Counter />
 		</div>
 	);
 }
---
+++ redux-tutorial/src/features/counter/counterSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+	count: 0,
+};
+
+export const counterSlice = createSlice({
+	name: "counter",
+	initialState,
+	reducers: {
+		//Actions
+		increment: (state) => {
+			state.count = state.count + 1;
+		},
+		decrement: (state) => {
+			state.count = state.count - 1;
+		},
+		reset: (state) => {
+			state.count = 0;
+		},
+		incrementByAmount: (state, action) => {
+			state.count = state.count + action.payload;
+		},
+	},
+});
+
+export const { increment, decrement, reset, incrementByAmount } =
+	counterSlice.actions;
+
+export default counterSlice.reducer;
